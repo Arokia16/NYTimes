5343d761bdadd3d9a9108a97fb16cf1148482b35
Arokia16 <sarath.jasrin@gmail.com>
2020-02-15 18:35:15 +0530

    Intial Commit

---
+++ precache-manifest.15098a8258af4937b7e5aa2f1fb9158f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043e432ff0353a593d7ab9b69260ace0",
+    "url": "/NYT/index.html"
+  },
+  {
+    "revision": "78cb049bafa2a2c9a8c1",
+    "url": "/NYT/static/js/2.d906eca3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/NYT/static/js/2.d906eca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0faf8d9824643966607",
+    "url": "/NYT/static/js/main.0de15b7c.chunk.js"
+  },
+  {
+    "revision": "62695e3115e83874d097",
+    "url": "/NYT/static/js/runtime-main.15f14fcc.js"
+  }
+]);
